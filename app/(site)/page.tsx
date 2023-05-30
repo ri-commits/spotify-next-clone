9f862bfda1342a76cdaef16699c57d5eb1c84f61
ri-commits <kartashourychard@gmail.com>
2023-05-30 17:42:21 +0300

    List Item

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,10 +13,25 @@ export default function Home() {
           2xl:grid-cols-4 gap-3 mt-4
           "
           >
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1
+            className="
+            text-white text-2xl font-semibold
+          "
+          >
+            Newest songs
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
